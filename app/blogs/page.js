33e6f9b145613d6e9ad84ef832a2c79347f022d8
Xiaotian Liu <cblxxlx@gmail.com>
2023-06-14 03:06:46 +1000

    fixed text left align

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,23 +1,19 @@
 import Link from 'next/link';
 import NavBar from '../components/NavBar';
-import { compareDesc, format, parseISO } from 'date-fns';
+import { compareDesc, format, parseISO, getYear } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
 
 function PostCard(post) {
   return (
-    <div className="flex flex-col justify-center items-center mb-8">
-      <h2 className="text-2xl hover:text-blue-500">
-        <Link href={post.slug} legacyBehavior>
-          {post.title}
-        </Link>
+    <div className="flex flex-col justify-center mb-8">
+      <h2 className="text-2xl hover:text-blue-500 mb-4">
+        <Link href={post.slug}>{post.title}</Link>
       </h2>
       <time dateTime={post.date} className="mb-2 text-sm text-gray-600">
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <p className="text-sm max-w-xs md:max-w-xl">
-        <Link href={post.slug} legacyBehavior>
-          {post.description}
-        </Link>
+      <p className="text-sm md:max-w-xl">
+        <Link href={post.slug}>{post.description}</Link>
       </p>
     </div>
   );
@@ -28,12 +24,34 @@ export default function BlogsPage() {
     compareDesc(new Date(a.date), new Date(b.date))
   );
 
+  // Group posts by year
+  const postsByYear = posts.reduce((acc, post) => {
+    const year = getYear(parseISO(post.date));
+    if (!acc[year]) {
+      acc[year] = [];
+    }
+    acc[year].push(post);
+    return acc;
+  }, {});
+
+  // Get the unique years in descending order
+  const years = Object.keys(postsByYear).sort((a, b) => b - a);
+
   return (
     <div>
       <NavBar />
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+      <div className="flex justify-center">
+        <div className="max-w-7xl">
+          {years.map((year) => (
+            <div key={year}>
+              <h2 className="text-2xl my-4 text-gray-400 max-w-7xl">{year}</h2>
+              {postsByYear[year].map((post, idx) => (
+                <PostCard key={idx} {...post} />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
