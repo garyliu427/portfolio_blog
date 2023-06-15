eed64580a616e82771a32c561972ca3a2b2656b5
Xiaotian Liu <cblxxlx@gmail.com>
2023-06-16 02:22:52 +1000

    fixed code overflow issue

--- app/components/MDXComponents.js
+++ app/components/MDXComponents.js
@@ -45,7 +45,10 @@ const MDXComponents = {
   td: (props) => <td className="border px-4 py-2" {...props} />,
   tr: (props) => <tr className="m-0 border-t p-0 even:bg-muted" {...props} />,
   pre: (props) => (
-    <pre className="p-5 rounded-lg border bg-code-gray py-4" {...props} />
+    <pre
+      className="p-5 rounded-lg overflow-x-auto border bg-code-gray py-4"
+      {...props}
+    />
   ),
   code: (props) => (
     <code className="relative py-[0.2rem] text-sm code" {...props} />
